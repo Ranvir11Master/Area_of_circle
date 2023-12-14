a5b062152e8ec1514a06560ffaaacf89ad44b64d
Ranvir11Master <master11ranvir@gmail.com>
2023-12-14 20:25:37 +0530

    add return

--- area.c
+++ area.c
@@ -1,9 +1,12 @@
 #include<stdio.h>
 
+//Finding Area Of circle
 int main() {
     float radius;
     printf("Enter radius : ");
     scanf("%f",&radius);
 
     printf("Area of circle : %f \n", 3.14 * radius * radius);
+
+    return 0;
 }
